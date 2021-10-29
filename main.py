8de8b76606c0c6248aa2cf165e1668cc07baa281
cleem <85625458+cleeem@users.noreply.github.com>
2021-10-29 16:45:37 +0000

    ajour commandes musiques fichier dependances

--- main.py
+++ main.py
@@ -1682,24 +1682,88 @@ def random_comp(n=8):
 
 
 
-from discord.ext.commands import Bot
-from discord.voice_client import VoiceClient
+import youtube_dl
 import asyncio
- 
-@bot.command(pass_context=True)
-async def join(ctx):
-    auteur = ctx.message.author
-    liste_salons = ctx.guild.voice_channels
-    for salon in liste_salons :
-        if auteur in salon.members :
-            await ctx.send("oui")
-            await salon.connect()
-        
-                            
+from discord import FFmpegPCMAudio
 
+musics = {}
+ytdl = youtube_dl.YoutubeDL()
+
+
+@bot.event
+async def on_ready():
+    print("Ready")
+
+
+class Video:
+    def __init__(self, link):
+        video = ytdl.extract_info(link, download=False)
+        video_format = video["formats"][0]
+        self.url = video["webpage_url"]
+        self.stream_url = video_format["url"]
+
+@bot.command()
+async def leave(ctx):
+    client = ctx.guild.voice_client
+    await client.disconnect()
+    musics[ctx.guild] = []
+
+@bot.command()
+async def resume(ctx):
+    client = ctx.guild.voice_client
+    if client.is_paused():
+        client.resume()
+
+
+@bot.command()
+async def pause(ctx , url = None):
+    client = ctx.guild.voice_client
+    if not client.is_paused():
+        client.pause()
+        await ctx.send("pause")
+    
+
+
+@bot.command()
+async def skip(ctx):
+    client = ctx.guild.voice_client
+    client.stop()
+
+
+def play_song(client, queue, song):
+    source = PCMVolumeTransformer(FFmpegPCMAudio(song.stream_url
+        , before_options = "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"))
+
+    def next(_):
+        if len(queue) > 0:
+            new_song = queue[0]
+            del queue[0]
+            play_song(client, queue, new_song)
+        else:
+            asyncio.run_coroutine_threadsafe(client.disconnect(), bot.loop)
+
+    client.play(source, after=next)
+
+
+@bot.command()
+async def play(ctx, url):
+    print("play")
+    client = ctx.guild.voice_client
+
+    if client and client.channel:
+        video = Video(url)
+        musics[ctx.guild].append(video)
+    else:
+        channel = ctx.author.voice.channel
+        video = Video(url)
+        musics[ctx.guild] = []
+        client = await channel.connect()
+        await ctx.send(f"Je lance : {video.url}")
+        play_song(client, musics[ctx.guild], video)  
+
+
+                            
 token = os.environ['token']
 token1 = os.environ['token1']
 
-
 bot.run(token1)
-
